414d3b877e2ff111d33c05bb2ad8566142d7c436
undeadlol1 <undeadlol1@mail.ru>
2017-11-14 21:33:44 +0300

    fix revisionsApi

--- src/server/middlewares/revisionsApi/revisionsApi.js
+++ src/server/middlewares/revisionsApi/revisionsApi.js
@@ -1,27 +1,24 @@
-import { mustLogin } from 'server/services/permissions'
-import { RevisionsApi } from 'server/data/models'
-import express from 'express'
 import slugify from 'slug'
+import { Router } from 'express'
+import { Revisions } from 'server/data/models'
+import { mustLogin } from 'server/services/permissions'
 
-// routes
-const router = express.Router()
 const limit = 12
 
-router
+export default Router()
 
-  // get all revisionsApis for index page
-  .get('/:page?', async (req, res) => { // TODO make sure pagination works right
+  // get all revisions
+  .get('/:page?', async (req, res) => {
     try {
       const page = req.params.page
       const offset = page ? limit * (page -1) : 0
-      const totalRevisionsApis = await RevisionsApi.count()
-      const totalPages = Math.ceil(totalRevisionsApis / limit)
-      const revisionsApis = await RevisionsApi.findAll({
+      const totalRevisionss = await Revisions.count()
+      const totalPages = Math.ceil(totalRevisionss / limit)
+      const revisions = await Revisions.findAll({
         limit,
         offset,
-        order: 'rand()',
       })
-      res.json({ revisionsApis, totalPages })
+      res.json({ revisions, totalPages })
     }
     catch (error) {
       console.log(error);
@@ -29,71 +26,41 @@ router
     }
   })
 
-  // get single revisionsApi by slug or name
-  .get('/revisionsApi/:slug?', async ({params, query}, res) => {
+  // get single revision
+  .get('/revision/:revisionsId', async ({params}, res) => {
     try {
-      const slug = params.slug
-      const name = query.name
-      const revisionsApi = await RevisionsApi.findOne({
-        where: {
-          $or: [{slug}, {name}]
-        }
-      })
-      res.json(revisionsApi)
+      const revision = await Revisions.findById(params.revisionsId)
+      res.json(revision)
     } catch (error) {
       console.log(error)
       res.status(500).end(error)
     }
   })
 
-  // search for revisionsApi
-  .get('/search/:name/:page?', async (req, res) => { // TODO make sure pagination works right
-    try {
-      const { page, name } = req.params
-      if (!name) return res.boom.badRequest('invalid query')
-      const offset = page ? limit * (page -1) : 0
-      const where = {
-                      name: { $like: '%' + name + '%' }
-                    }
-      const totalRevisionsApis = await RevisionsApi.count({ where })
-      const totalPages = Math.round(totalRevisionsApis / limit)
-      const revisionsApis = await RevisionsApi.findAll({
-        limit,
-        offset,
-        where,
-      }) || []
-      res.json({ revisionsApis, totalPages })
-    }
-    catch (error) {
-      console.log(error);
-      res.status(500).end(error)
-    }
-  })
-
-  // create revisionsApi
-  .post('/', mustLogin, async ({user, body: { name }}, res) => {
+  // update revision
+  .put('/:revisionsId', mustLogin, async ({user, body, params}, res) => {
     try {
       const UserId = user.id
-      const slug = slugify(name)
-      const revisionsApi = await RevisionsApi.create({ UserId, name, slug }) // TODO move this in model definition?
-      res.json(revisionsApi)
+      const revision = await Revisions.findById(params.revisionsId)
+
+      // check permissions
+      if (Revisions.UserId != UserId) return res.status(401).end()
+      else res.json(await revision.update(body))
+
     } catch (error) {
       console.log(error)
       res.status(500).end(error)
     }
   })
 
-  // create revisionsApi
-  .post('/', mustLogin, async ({user, body: { name }}, res) => {
+  // create revision
+  .post('/', mustLogin, async ({user, body}, res) => {
     try {
       const UserId = user.id
-      const slug = slugify(name)
-      const revisionsApi = await RevisionsApi.create({ UserId, name, slug }) // TODO move this in model definition?
-      res.json(revisionsApi)
+      const revision = await Revisions.create({...body, UserId})
+      res.json(revision)
     } catch (error) {
       console.log(error)
       res.status(500).end(error)
     }
   })
-
-export default router
--- src/server/middlewares/revisionsApi/revisionsApi.test.js
+++ src/server/middlewares/revisionsApi/revisionsApi.test.js
@@ -2,19 +2,19 @@ import 'babel-polyfill'
 import chai from 'chai'
 import slugify from 'slug'
 import request from 'supertest'
-import server from 'src/server'
-import users from 'src/server/data/fixtures/users'
-import { RevisionsApi, User } from 'src/server/data/models'
-import { loginUser } from 'src/server/test/middlewares/authApi.test'
+import server from 'server/server'
+import users from 'server/data/fixtures/users'
+import { Revisions, User } from 'server/data/models'
+import { loginUser } from 'server/test/middlewares/authApi.test'
 chai.should();
 
 const   user = request.agent(server),
         username = users[0].username,
         password = users[0].password,
-        revisionsRevisionsApi = "random name",
-        slug = slugify(revisionsRevisionsApi)
+        revision = "random name",
+        slug = slugify(revision)
 
-export default describe('/revisionsApis API', function() {
+export default describe('/revisions API', function() {
 
     before(async function() {
         // TODO add logout? to test proper user login?
@@ -25,13 +25,13 @@ export default describe('/revisionsApis API', function() {
 
     // clean up
     after(function() {
-        RevisionsApi.destroy({where: { name: revisionsRevisionsApi }})
+        Revisions.destroy({where: { name: revision }})
     })
 
-    it('POST revisionsApi', async function() {
+    it('POST revision', async function() {
         const agent = await loginUser(username, password)
-        await agent.post('/api/revisionsApis')
-            .send({ name: revisionsRevisionsApi })
+        await agent.post('/api/revisions')
+            .send({ name: revision })
             .expect('Content-Type', /json/)
             .expect(200)
             .then(function(res) {
@@ -43,46 +43,37 @@ export default describe('/revisionsApis API', function() {
             })
     })
 
-    it('GET revisionsApis', function(done) {
+    it('GET revisions', function(done) {
         request(server)
-            .get('/api/revisionsApis')
+            .get('/api/revisions')
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function(err, res) {
                 if (err) return done(err);
-                res.body.revisionsApis.should.be.a('array')
+                res.body.revisions.should.be.a('array')
                 done()
             });
     })
 
-    it('GET single revisionsApi', function(done) {
+    it('GET single revision', function(done) {
         user
-            .get('/api/revisionsApis/revisionsApi/' + slug )
+            .get('/api/revisions/revision/' + slug )
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function(err, res) {
                 if (err) return done(err);
-                res.body.name.should.be.equal(revisionsRevisionsApi)
+                res.body.name.should.be.equal(revision)
                 done()
             });
     })
 
-    it('GET /search revisionsApis', function(done) {
-        user
-            .get('/api/revisionsApis/search/' + 'something' )
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                res.body.revisionsApis.should.be.a('array')
-                done()
-            });
-    })
+    // TODO PUT test
+
     // TODO create test for "mustLogin" function and this kind of tests will be obsolete
     it('fail to POST if not authorized', function(done) { // TODO move this to previous function?
         user
-            .post('/api/revisionsApis')
-            .send({ name: revisionsRevisionsApi })
+            .post('/api/revisions')
+            .send({ name: revision })
             .expect(401, done)
     })
 
