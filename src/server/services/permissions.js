87bb1aff58a88d311ea8855a0735cab0df7f3c2e
Mihail Paley <undeadlol1@mail.ru>
2017-12-08 15:34:23 +0500

    (permissons) add console logging

--- src/server/services/permissions.js
+++ src/server/services/permissions.js
@@ -7,5 +7,8 @@ import Boom from 'boom';
  * @returns
  */
 export function mustLogin(req, res, next) {
+    console.log('req.isAuthenticated: ', req.isAuthenticated);
+    console.log('req.user.id: ', req.user.id);
+    console.log('req.session: ', req.session);
     req.isAuthenticated() ? next() : res.boom.unauthorized('Please, log in to do this')
 }
