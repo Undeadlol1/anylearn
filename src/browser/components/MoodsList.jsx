39bd29aa0320c39dc5252d0354fe0ac8afd441d2
undeadlol1 <undeadlol1@mail.ru>
2017-12-31 01:34:40 +0300

    (MoodsList) fix styles

--- src/browser/components/MoodsList.jsx
+++ src/browser/components/MoodsList.jsx
@@ -29,8 +29,8 @@ export class MoodsList extends Component {
 					return	<Col className="MoodsList__item" style={itemStyles} xs={12} sm={6} md={4} lg={4} key={skill.get('id')}>
 								<Paper zDepth={5}>
 									<Link to={`/skill/${skill.get('slug')}`}>
-										<Card>
-											<CardMedia overlay={<CardTitle title={skill.get('name')} />}>
+										<Card className="MoodsList__card">
+											<CardMedia overlay={<CardTitle title={skill.get('name')} className="MoodsList__imgContainer" />}>
 												<img alt={skill.get('name') + translate('things_image')} src={src} />
 											</CardMedia>
 										</Card>
