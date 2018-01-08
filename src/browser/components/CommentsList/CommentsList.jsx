46429fd411cd517672700f56075d40af26c99126
Mihail Paley <undeadlol1@mail.ru>
2018-01-08 06:45:13 +0500

    (CommentsList) refactor

--- src/browser/components/CommentsList/CommentsList.jsx
+++ src/browser/components/CommentsList/CommentsList.jsx
@@ -2,7 +2,6 @@ import cls from 'classnames'
 import PropTypes from 'prop-types'
 import Paper from 'material-ui/Paper'
 import { connect } from 'react-redux'
-// import { VK, Comments } from 'react-vk'
 import React, { Component } from 'react'
 import { Row, Col } from 'react-styled-flexboxgrid'
 import { translate as t } from 'browser/containers/Translator'
@@ -10,20 +9,13 @@ import { translate as t } from 'browser/containers/Translator'
 class CommentsList extends Component {
 	componentDidMount() {
 		if (process.env.BROWSER) {
-			console.log('componentDidMount browser');
 			VK.init({
 				onlyWidgets: true,
 				apiId: process.env.VK_ID,
 			})
-			// window.url
-			console.log('window.url: ', window.URL);
-			window.location.href
-			console.log('window.location.href : ', window.location.href );
 			VK.Widgets.Comments(
 				'vk_comments',
-				{
-					pageUrl: window.location.href,
-				},
+				{pageUrl: window.location.href},
 				window.location.href
 			)
 		}
@@ -34,15 +26,7 @@ class CommentsList extends Component {
 		return 	<Row className={className}>
 					<Col xs={12}>
 						<Paper zDepth={3}>
-							{
-								process.env.BROWSER
-								&&
-								<div id="vk_comments"></div>
-								// && 	<VK apiId={Number(process.env.VK_ID)}>
-								// 		<Comments />
-								// 		{/* onNewComment={() => console.log('1')}  */}
-								// 	</VK>
-							}
+							<div id="vk_comments" />
 						</Paper>
 					</Col>
 				</Row>
