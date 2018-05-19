752f26810d21538fb560ea3052f2fc53ed78dcc6
Mihail Paley <undeadlol1@mail.ru>
2018-05-19 21:06:26 +0500

    (models) add comments to revisions and skills

--- src/server/data/models/revisions.js
+++ src/server/data/models/revisions.js
@@ -6,33 +6,40 @@ module.exports = function(sequelize, DataTypes) {
       primaryKey: true,
       type: DataTypes.UUID,
       validate: { isUUID: 4 },
+      comment: 'Unique identifier.',
       defaultValue: DataTypes.UUIDV4,
     },
     name: {
         allowNull: false,
         type: DataTypes.STRING,
+        comment: 'Short comment about made change.',
     },
     text: {
         allowNull: false,
         type: DataTypes.TEXT('long'),
+        comment: 'Full text of changed document.',
     },
     description: {
         allowNull: true,
         type: DataTypes.STRING,
+        comment: 'Long comment about made change.',
     },
     UserId: {
       allowNull: false,
       type: DataTypes.INTEGER,
+      comment: 'Unique id of a user who created this revision.',
     },
     active: {
       allowNull: false,
       defaultValue: true,
       type: DataTypes.BOOLEAN,
+      comment: 'Is this revision revision the current one or not.',
     },
     // TODO do we need this?
     image: {
       allowNull: true,
       type: DataTypes.STRING,
+      comment: 'Besides text revisions can also change images.',
       // validate: {
       //   isUrl: true,
       //   notEmpty: true,
@@ -41,16 +48,20 @@ module.exports = function(sequelize, DataTypes) {
     parentId: {
       allowNull: false,
       type: DataTypes.UUID,
+      // TODO: add validate: UUID/v4
+      comment: 'Unique identifier of parent document. Most likely id of a skill.',
     },
     previousId: {
       allowNull: true, // ?????
       type: DataTypes.UUID,
+      comment: 'Id of previous revision. This is needed to be able to cycle through revisions.',
     },
     // TODO do we need this?
     rating: {
       defaultValue: 0,
       allowNull: false,
       type: DataTypes.STRING,
+      comment: 'The amount of likes/dislikes revision has.',
     },
   }, {
       tableName: 'revisions',
--- src/server/data/models/skills.js
+++ src/server/data/models/skills.js
@@ -6,36 +6,43 @@ module.exports = function(sequelize, DataTypes) {
       primaryKey: true,
       type: DataTypes.UUID,
       validate: { isUUID: 4 },
+      comment: 'Unique identifier.',
       defaultValue: DataTypes.UUIDV4,
     },
     name: {
         unique: true,
         allowNull: false,
         type: DataTypes.STRING,
+        comment: 'Human readable name for a skill.',
     },
     slug: {
         unique: true,
         allowNull: false,
         type: DataTypes.STRING,
+        comment: 'Url friendly name of a skill.',
     },
     UserId: {
       allowNull: false,
       type: DataTypes.INTEGER,
+      comment: 'Unique id of a user who created this skill.',
     },
     RevisionId: {
       allowNull: false,
       type: DataTypes.UUID,
+      comment: 'Id of current active revision. Probably not used ATM.',
     },
     // TODO do we need this?
     image: {
       allowNull: true,
       type: DataTypes.STRING,
+      comment: 'This skills image url.',
     },
     // TODO do we even use this?
     rating: {
       allowNull: false,
       defaultValue: 0,
       type: DataTypes.INTEGER,
+      comment: 'The amount of likes/dislikes this document have.',
     },
   }, {
       tableName: 'skills',
